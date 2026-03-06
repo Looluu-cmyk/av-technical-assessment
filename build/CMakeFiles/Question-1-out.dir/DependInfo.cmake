520b36b873239ea4d9aef371505a396e419eb59e
Dzan Au <z5308353@ad.unsw.edu.au>
2026-03-06 18:00:37 +1100

    setup cmake

---
+++ build/CMakeFiles/Question-1-out.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nzk0/redback/av-technical-assessment/Q1/Question-1.cc" "CMakeFiles/Question-1-out.dir/Q1/Question-1.cc.o" "gcc" "CMakeFiles/Question-1-out.dir/Q1/Question-1.cc.o.d"
+  "" "Question-1-out" "gcc" "CMakeFiles/Question-1-out.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Question-2-out.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nzk0/redback/av-technical-assessment/Q2/Question-2.cc" "CMakeFiles/Question-2-out.dir/Q2/Question-2.cc.o" "gcc" "CMakeFiles/Question-2-out.dir/Q2/Question-2.cc.o.d"
+  "" "Question-2-out" "gcc" "CMakeFiles/Question-2-out.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
